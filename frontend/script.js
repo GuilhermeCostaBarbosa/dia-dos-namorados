6d22fa4a20db1bb96c08b560c0b41ee97c6be15d
Guilherme_Costa_Barbosa <costabarbosag2@gmail.com>
2025-06-10 14:00:01 -0300

    Update script.js
    Adicionado a função loveTimer, responsável para manter o timer funcionando na página.

--- frontend/script.js
+++ frontend/script.js
@@ -21,3 +21,23 @@ const swiper = new Swiper(".swiper", {
       }
     }
 });
+
+let timer = document.getElementById('timer')
+
+function loveTimer(){
+  
+  const intervalo = setInterval(() => {
+    let agora = new Date()
+    const diff = agora - dataInicial
+    const days = Math.floor(diff / (1000 * 60 * 60 * 24))
+    const hours = Math.floor((diff % (1000 * 60 * 60 * 24))/ (1000 * 60 * 60)) 
+    const minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60))
+    const seconds = Math.floor((diff % (1000 * 60)) / 1000)
+    
+    timer.innerHTML = `<p>${days} dias, ${hours} horas, ${minutes} minutos e ${seconds} segundos...</p>`
+  
+  }, 1000)
+}
+
+let dataInicial = new Date('2024-02-25T00:00:00')
+loveTimer()
