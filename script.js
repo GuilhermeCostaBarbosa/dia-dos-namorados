3c26030eee8867749dc1d689122e2bd24b1b7671
Guilherme_Costa_Barbosa <costabarbosag2@gmail.com>
2025-06-10 23:41:28 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
+// Biblioteca Swiper para carrosel de imagens
 const swiper = new Swiper(".swiper", {
     spaceBetween: 30,
     centeredSlides: true,
     loop: true,
-    //   autoplay: {
-    //     delay: 2500,
-    //     disableOnInteraction: false,
-    //   },
+    autoplay: {
+      delay: 5000,
+      sableOnInteraction: false,
+      },
     pagination: {
       el: ".swiper-pagination",
       clickable: true,
@@ -22,6 +23,7 @@ const swiper = new Swiper(".swiper", {
     }
 });
 
+// Timer do tempo de namoro
 let timer = document.getElementById('timer')
 
 function loveTimer(){
@@ -34,10 +36,30 @@ function loveTimer(){
     const minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60))
     const seconds = Math.floor((diff % (1000 * 60)) / 1000)
     
-    timer.innerHTML = `<p>${days} dias, ${hours} horas, ${minutes} minutos e ${seconds} segundos...</p>`
+    timer.innerHTML = `<p>${days} dias, ${hours} horas, ${minutes} minutos e<br>${seconds} segundos...</p>`
   
   }, 1000)
 }
 
 let dataInicial = new Date('2024-02-25T00:00:00')
 loveTimer()
+
+// Encaminhar mensagem no WhatsApp conforme texto no textarea
+const whatsappBtn = document.getElementById('whatsapp-btn')
+
+whatsappBtn.addEventListener('click', () => {
+  const numero = '5515991713819'
+  const mensagem = document.getElementById('mensagem-amor').value
+
+  if (mensagem.trim() === ''){
+    alert('Vai mandar nenhuma mensagem para mim amor...  :(')
+    return
+  }
+
+  const textoCodificado = encodeURIComponent(mensagem)
+  const link = `https://wa.me/${numero}?text=${textoCodificado}`
+
+  window.open(link, '_blank')
+
+})
+
